7d5721d88eed761a1db242f0ad28c0e54b95bfb3
coryhas <cory.kurilov@gmail.com>
2020-03-10 12:53:50 -0700

    added footer

--- contact.js
+++ contact.js
@@ -1,11 +1,15 @@
-// Validating Empty Field
+//create an alert IF there are empty fields
 function check_empty() {
     if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
     alert("Fill All Fields !");
-    } else {
+    } 
+
+    else {
     document.getElementById('form').submit();
     alert("Form Submitted Successfully...");
     }
+    //create console log to print form results
+    //console.log ('form') ;//
     }
     //Function To Display Popup
     function div_show() {

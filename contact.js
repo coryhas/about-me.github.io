60254c157d16c23517783563375d1ab1e4b4cef7
coryhas <cory.kurilov@gmail.com>
2020-03-10 20:01:42 -0700

    fixed pop uplooksworking on all plat forms

--- contact.js
+++ contact.js
@@ -1,21 +1,19 @@
 //create an alert IF there are empty fields
 function check_empty() {
     if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-    alert("Fill All Fields !");
-    } 
+        alert("Fill All Fields !");
+    }
 
     else {
-    document.getElementById('form').submit();
-    alert("Form Submitted Successfully...");
+        document.getElementById('form').submit();
+        alert("Form Submitted Successfully...");
     }
-    //create console log to print form results
-    //console.log ('form') ;//
-    }
-    //Function To Display Popup
-    function div_show() {
+     }
+//Function To Display Popup
+function div_show() {
     document.getElementById('abc').style.display = "block";
-    }
-    //Function to Hide Popup
-    function div_hide(){
+}
+//Function to Hide Popup
+function div_hide() {
     document.getElementById('abc').style.display = "none";
-    }
+} 
